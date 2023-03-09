f778116e19cde0efb7716dc1fc2fb90ab8aeaaa5
Nelson <antonionelson.adsi@gmail.com>
2023-03-09 19:38:55 -0300

    Initial commit

---
+++ js/headerclick.js
@@ -0,0 +1,16 @@
+$("#item1").click(function () {
+    $("#item1").toggleClass("fa-minus fa-plus");
+
+});
+$("#item2").click(function () {
+    $("#item2").toggleClass("fa-plus fa-minus");
+
+});
+$("#item3").click(function () {
+    $("#item3").toggleClass("fa-plus fa-minus");
+
+});
+$("#item4").click(function () {
+    $("#item3").toggleClass("fa-minus fa-plus");
+
+});
